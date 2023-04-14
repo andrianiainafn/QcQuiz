2f1a564837fee29595e54a6f0fe9cd235ad7dad8
andrianiainafn <107101235+andrianiainafn@users.noreply.github.com>
2023-04-14 14:28:08 +0300

    final oush

--- client/src/features/Admin/components/elements/Topbar.jsx
+++ client/src/features/Admin/components/elements/Topbar.jsx
@@ -8,18 +8,31 @@ import  SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
 import  PersonOutlinedIcon from '@mui/icons-material/PersonOutlined'
 import  SearchIcon from '@mui/icons-material/Search'
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 function Topbar() {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const HadleClickSearch = ()=>{
+    navigate('/admin/dashboard/student/search')
+  }
   return (
     <Box display="flex" justifyContent="space-between" py={2} style={{width:'99%'}} >
          {/* //Search bar */}
-         <div display="flex" className='flex bg-transparent rounded-lg border border-[#66ACFF]' 
-         >
-          <InputBase sx={{ml:2,flex:1,color:'#66ACFF'}} placeholder="Search"/>
-          <IconButton type="button" sx={{p:1}}>
-            <SearchIcon sx={{color: "#66ACFF"}}/>
-          </IconButton>
+         {
+           !(location.pathname === "/admin/dashboard/student/search") ? (
+            <div display="flex" className='flex bg-transparent rounded-lg border border-[#66ACFF]' >
+            <InputBase onClick={HadleClickSearch} sx={{ml:2,flex:1,color:'#66ACFF'}} placeholder="Search"/>
+            <IconButton  type="button" sx={{p:1}}>
+              <SearchIcon sx={{color: "#66ACFF"}}/>
+            </IconButton>
          </div>
+           ) :(
+            <div>
+               <h3 className='text-[#66ACFF] text-2xl'>Search</h3>
+            </div>
+           )
+         }
           {/* Icons */}
           <Box display="flex" >
           <IconButton>
