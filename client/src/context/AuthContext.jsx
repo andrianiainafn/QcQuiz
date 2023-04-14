f151b58e71788799405067e38c18264fe772fc93
andrianiainafn <107101235+andrianiainafn@users.noreply.github.com>
2023-04-14 16:14:09 +0300

    final push

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -8,6 +8,7 @@ function AuthContext({children}) {
   const [isConnected,setIsConnected] = useState(false) 
   const [studentMatricule,setStudentMatricule] = useState()
   const [studentNiveau,setStudentNiveau] = useState()
+  const [studentName,setStudentName] = useState()
   const getIsConnected = async ()=>{
     const connection = await axios.get('http://localhost:8080/Auth/verifySession.php',{withCredentials:true})
     console.log(connection.data)
@@ -17,13 +18,14 @@ function AuthContext({children}) {
           setStudentMatricule(connection.data.matricule)
           console.log(connection.data.matricule,8000)
           setStudentNiveau(connection.data.niveau)
+          setStudentName(connection.data.name)
        }
     }
     useEffect(()=>{
       getIsConnected()
     },[])
   return (
-    <Context.Provider value={{isConnected,studentMatricule,studentNiveau,getIsConnected}}>
+    <Context.Provider value={{isConnected,studentMatricule,studentNiveau,getIsConnected,studentName}}>
         {children}
     </Context.Provider>
   )
